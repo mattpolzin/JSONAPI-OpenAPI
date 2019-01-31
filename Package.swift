8bf5daba023b1aebfaedd4a8a4638daf1610621f
Mathew Polzin <matt.polzin@gmail.com>
2019-01-30 17:53:34 -0800

    Update Package description

--- Package.swift
+++ Package.swift
@@ -12,17 +12,18 @@ let package = Package(
             targets: ["JSONAPIOpenAPI"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Flight-School/AnyCodable.git", from: "0.1.0"),
+        .package(url: "https://github.com/mattpolzin/Sampleable.git", from: "1.0.0"),
+        .package(url: "https://github.com/mattpolzin/JSONAPI.git", from: "0.18.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "JSONAPIOpenAPI",
-            dependencies: []),
-        .testTarget(
-            name: "JSONAPIOpenAPITests",
-            dependencies: ["JSONAPIOpenAPI"]),
+            dependencies: ["JSONAPI", "AnyCodable", "JSONAPIArbitrary", "Sampleable"]),
+            .testTarget(
+                name: "JSONAPIOpenAPITests",
+                dependencies: ["JSONAPI", "JSONAPIOpenAPI"])
     ]
 )
