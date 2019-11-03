a9c91a22b9f6ac769041aefed893bdd2b8d44836
Mathew Polzin <matt.polzin@gmail.com>
2019-11-02 17:19:40 -0700

    Add new target's test files

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,8 +1,10 @@
 import XCTest
 
 import JSONAPIOpenAPITests
+import JSONAPISwiftGenTests
 
 var tests = [XCTestCaseEntry]()
 tests += JSONAPIOpenAPITests.__allTests()
+tests += JSONAPISwiftGenTests.__allTests()
 
 XCTMain(tests)
