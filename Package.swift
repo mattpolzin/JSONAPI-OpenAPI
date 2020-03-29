786d35ba41124ec89d94b3d4818e7deb54fbe669
Mathew Polzin <matt.polzin@gmail.com>
2020-03-29 13:15:05 -0700

    lock down OpenAPIReflection version.

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .package(url: "https://github.com/mattpolzin/Sampleable.git", .upToNextMajor(from: "2.0.0")),
         .package(url: "https://github.com/mattpolzin/JSONAPI.git", .upToNextMajor(from: "3.0.0")),
         .package(url: "https://github.com/mattpolzin/OpenAPIKit.git", .upToNextMinor(from: "0.23.0")),
-        .package(url: "https://github.com/mattpolzin/OpenAPIReflection.git", .upToNextMinor(from: "0.0.1")),
+        .package(url: "https://github.com/mattpolzin/OpenAPIReflection.git", .exact("0.0.1")),
         .package(url: "https://github.com/typelift/SwiftCheck.git", .upToNextMinor(from: "0.12.0"))
     ],
     targets: [
