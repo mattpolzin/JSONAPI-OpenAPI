489605673c9134e64159222bf2152ec62a74138b
Mathew Polzin <matt.polzin@gmail.com>
2026-02-20 10:29:01 -0600

    update dependencies. remove broken readme badge

--- Package.swift
+++ Package.swift
@@ -22,11 +22,11 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/mattpolzin/Sampleable.git", from: "2.0.0"),
         .package(url: "https://github.com/mattpolzin/JSONAPI.git", from: "6.0.0"),
-        .package(url: "https://github.com/mattpolzin/OpenAPIKit.git", from: "5.0.0-rc.2"),
-        .package(url: "https://github.com/mattpolzin/OpenAPIReflection.git", branch: "feature/openapikit-5"),
+        .package(url: "https://github.com/mattpolzin/OpenAPIKit.git", from: "5.0.0"),
+        .package(url: "https://github.com/mattpolzin/OpenAPIReflection.git", from: "4.0.0"),
         .package(url: "https://github.com/typelift/SwiftCheck.git", .upToNextMinor(from: "0.12.0")),
-        .package(url: "https://github.com/apple/swift-format.git", from: "509.0.0"),
-        .package(url: "https://github.com/pointfreeco/swift-nonempty.git", .upToNextMinor(from: "0.2.0")),
+        .package(url: "https://github.com/apple/swift-format.git", from: "602.0.0"),
+        .package(url: "https://github.com/pointfreeco/swift-nonempty.git", .upToNextMinor(from: "0.5.0")),
         .package(url: "https://github.com/mattpolzin/JSONAPIViz.git", exact: "0.0.7")
     ],
     targets: [
